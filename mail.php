6b63920ccab1b7d4031574a15a794c154408f613
shahzod98 <60297568+shahzod98@users.noreply.github.com>
2020-02-13 14:13:20 +0500

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['text'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'shahzod998@inbox.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'qoplon98'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('shahzod998@inbox.ru'); // от кого будет уходить письмо?
+$mail->addAddress('gulinora1978@gmail.com');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    header('location: thank.html');
+    // echo 'Error dsdsd';
+} else {
+    //header('location: thank-you.html');
+    echo 'thank you!!!';
+
+}
+?>
